0e2be1990a153b468017b797167a81e5dc75b800
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2025-09-04 13:05:15 +0800

    revert

--- js/GameBoyCore.js
+++ js/GameBoyCore.js
@@ -4609,7 +4609,7 @@ GameBoyCore.prototype.ROMLoad = function () {
 	this.usedBootROM = settings[1] && ((!settings[11] && this.GBCBOOTROM.length == 0x800) || (settings[11] && this.GBBOOTROM.length == 0x100));
 	var maxLength = this.ROMImage.length;
 	if (maxLength < 0x4000) {
-		// throw(new Error("ROM image size too small."));
+		throw(new Error("ROM image size too small."));
 	}
 	this.ROM = this.getTypedArray(maxLength, 0, "uint8");
 	var romIndex = 0;
